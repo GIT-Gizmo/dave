c05807b0c66f3747c05ee683ebd1b68650d9e6c9
David Bolu <66206100+GIT-Gizmo@users.noreply.github.com>
2023-05-19 20:38:03 +0100

    Update Info.js

--- src/assets/info/Info.js
+++ src/assets/info/Info.js
@@ -111,7 +111,7 @@ export const info = {
       title: 'Glamour Threads',
       live: 'https://glamour-threads.vercel.app/', //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
       source: 'https://github.com/GIT-Gizmo/glamour-threads', // this should be a link to the **repository** of the project, where the code is hosted.
-      image: 'https://dave-dev.vercel.app/assets/img/glamour-threads.png',
+      image: '../img/glamour-threads.png',
     }
   ],
 };
