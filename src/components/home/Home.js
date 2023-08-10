dc94784cc22ccb966e53b6e7ce6d973ced5b6a2b
GIT-Gizmo <boluforjesus@gmail.com>
2023-08-10 17:27:34 +0100

    self image width adjustment

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -23,8 +23,8 @@ export default function Home({ darkMode }) {
         style={{ background: info.gradient, objectFit: 'cover' }}
         component={'img'}
         src={me}
-        width={{ xs: '35vh', md: '40vh' }}
-        height={{ xs: '35vh', md: '40vh' }}
+        width={{ xs: '25vh', md: '40vh' }}
+        height={{ xs: '25vh', md: '40vh' }}
         borderRadius={'50%'}
         p={'0.75rem'}
         mb={{ xs: '1rem', sm: 0 }}
