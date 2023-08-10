33e5728d694bac3e2c21fb7ab642cc3c331d9243
GIT-Gizmo <boluforjesus@gmail.com>
2023-08-10 17:38:39 +0100

    self info width adjustment

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -12,7 +12,7 @@ export default function Home({ darkMode }) {
     <Box
       component={'main'}
       display={'flex'}
-      flexDirection={{ xs: 'column', md: 'column', lg:'row' }}
+      flexDirection={{ xs: 'column', md: 'row' }}
       alignItems={'center'}
       justifyContent={'center'}
       minHeight={'calc(100vh - 175px)'}
@@ -23,8 +23,8 @@ export default function Home({ darkMode }) {
         style={{ background: info.gradient, objectFit: 'cover' }}
         component={'img'}
         src={me}
-        width={{ xs: '10vh', md: '40vh' }}
-        height={{ xs: '10vh', md: '40vh' }}
+        width={{ xs: '35vh', md: '40vh' }}
+        height={{ xs: '35vh', md: '40vh' }}
         borderRadius={'50%'}
         p={'0.75rem'}
         mb={{ xs: '1rem', sm: 0 }}
@@ -42,8 +42,6 @@ export default function Home({ darkMode }) {
           padding: '1rem 2rem',
           transition: 'all 0.3s ease',
         }}
-        width={{ xs: '35vh' }}
-        height={{ xs: '35vh' }}
       >
         <h1>
           Hi, I'm{' '}
@@ -56,7 +54,7 @@ export default function Home({ darkMode }) {
           >
             {info.firstName}
           </span>
-          <span className={Style.hand}>👋</span>
+          <span className={Style.hand}>🤚</span>
         </h1>
         <h2>I'm {info.position}.</h2>
         <Box component={'ul'} p={'0.8rem'}>
@@ -78,3 +76,75 @@ export default function Home({ darkMode }) {
     </Box>
   );
 }
+
+// export default function Home({ darkMode }) {
+//   return (
+//     <Box
+//       component={'main'}
+//       display={'flex'}
+//       flexDirection={{ xs: 'column', md: 'column', lg:'row' }}
+//       alignItems={'center'}
+//       justifyContent={'center'}
+//       minHeight={'calc(100vh - 175px)'}
+//     >
+//       <Box
+//         className={classNames(Style.avatar, Style.shadowed)}
+//         alt={'image of developer'}
+//         style={{ background: info.gradient, objectFit: 'cover' }}
+//         component={'img'}
+//         src={me}
+//         width={{ xs: '10vh', md: '40vh' }}
+//         height={{ xs: '10vh', md: '40vh' }}
+//         borderRadius={'50%'}
+//         p={'0.75rem'}
+//         mb={{ xs: '1rem', sm: 0 }}
+//         mr={{ xs: 0, md: '2rem' }}
+//       />
+//       <Box
+//         style={{
+//           borderRadius: '1rem',
+//           background: darkMode ? 'rgba(0, 0, 0, 0.2)' : 'rgba(255, 255, 255, 0.2)',
+//           boxShadow: darkMode
+//             ? '0 0 10px 0 rgba(0, 0, 0, 0.2)'
+//             : '0 0 10px 0 rgba(255, 255, 255, 0.2)',
+//           backdropFilter: 'blur(8px)',
+//           WebkitBackdropFilter: 'blur(8px)',
+//           padding: '1rem 2rem',
+//           transition: 'all 0.3s ease',
+//         }}
+//         width={{ xs: '35vh' }}
+//         height={{ xs: '35vh' }}
+//       >
+//         <h1>
+//           Hi, I'm{' '}
+//           <span
+//             style={{
+//               background: info.gradient,
+//               WebkitBackgroundClip: 'text',
+//               WebkitTextFillColor: 'transparent',
+//             }}
+//           >
+//             {info.firstName}
+//           </span>
+//           <span className={Style.hand}>👋</span>
+//         </h1>
+//         <h2>I'm {info.position}.</h2>
+//         <Box component={'ul'} p={'0.8rem'}>
+//           {info.miniBio.map((bio, index) => (
+//             <EmojiBullet key={index} emoji={bio.emoji} text={bio.text} />
+//           ))}
+//         </Box>
+//         <Box
+//           display={'flex'}
+//           gap={'1.5rem'}
+//           justifyContent={'center'}
+//           fontSize={{ xs: '2rem', md: '2.5rem' }}
+//         >
+//           {info.socials.map((social, index) => (
+//             <SocialIcon key={index} link={social.link} icon={social.icon} label={social.label} />
+//           ))}
+//         </Box>
+//       </Box>
+//     </Box>
+//   );
+// }
